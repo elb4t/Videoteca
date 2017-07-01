c24088c47f17662461b6641cbebed013b3f0c97d
Eloy Llacer <eloy4t@gmail.com>
2017-07-01 17:43:13 +0200

    Crear recomendaciones

---
+++ app/src/main/java/es/elb4t/videoteca/BootupActivity.java
@@ -0,0 +1,34 @@
+package es.elb4t.videoteca;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by eloy on 1/7/17.
+ */
+
+public class BootupActivity extends BroadcastReceiver {
+    private static final long INTERVAL = 30000;
+    private static final long INITIAL_DELAY = 5000;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().endsWith(Intent.ACTION_BOOT_COMPLETED)) {
+            scheduleRecommendationUpdate(context);
+        }
+    }
+
+    private void scheduleRecommendationUpdate(Context context) {
+        AlarmManager alarmManager = (AlarmManager)
+                context.getSystemService(Context.ALARM_SERVICE);
+        Intent recommendationIntent = new Intent(context,
+                UpdateRecommendationsService.class);
+        PendingIntent alarmIntent = PendingIntent.getService(context, 0,
+                recommendationIntent, 0);
+        alarmManager.setInexactRepeating(AlarmManager
+                .ELAPSED_REALTIME_WAKEUP, INITIAL_DELAY, INTERVAL, alarmIntent);
+    }
+}
